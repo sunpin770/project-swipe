fba5366baeff6403f1faf84292ae19cd77477bf3
Srulik P <srulikpinson770@gmail.com>
2025-03-16 12:52:10 -0600

    aaaaand... fixed

--- src/pieces/type.js
+++ src/pieces/type.js
@@ -1,42 +1,42 @@
 import { Color } from 'excalibur';
 import { Piece } from './piece.js';
 export class Kai extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#FFA500'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#FFA500'), pos);
     };
 };
 export class Sakal extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#FF0000'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#FF0000'), pos);
     };
 };
 export class Rui extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#008000'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#008000'), pos);
     };
 };
 export class Jar extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#FFFFFF'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#FFFFFF'), pos);
     };
 };
 export class Plumoni extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#FFFF00'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#FFFF00'), pos);
     };
 };
 export class Mras extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#0000ff'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#0000ff'), pos);
     };
 };
 export class Khyii extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#800080'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#800080'), pos);
     };
 };
 export class Tyern extends Piece {
-    constructor(name, pos) {
-        super(name, Color.fromHex('#Ff007f'), pos);
+    constructor(x, y, name, pos) {
+        super(x, y, name, Color.fromHex('#Ff007f'), pos);
     };
 };
