7cdf17b1332569ba822338b56050b7091739cbfc
Srulik P <srulikpinson770@gmail.com>
2025-03-15 21:53:16 -0600

    Engine configuration

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import { MyLevel } from "./level.js";
 // Goal is to keep main.ts small and just enough to configure the engine
 
 const game = new Engine({
-  width: 800, // Logical width and height in game pixels
-  height: 600,
-  displayMode: DisplayMode.FitScreenAndFill, // Display mode tells excalibur how to fill the window
-  pixelArt: true, // pixelArt will turn on the correct settings to render pixel art without jaggies or shimmering artifacts
+  width: 1000, // Logical width and height in game pixels
+  height: 1000,
+  displayMode: DisplayMode.Fixed, // Display mode tells excalibur how to fill the window
+  // pixelArt: true, // pixelArt will turn on the correct settings to render pixel art without jaggies or shimmering artifacts
   scenes: {
     start: MyLevel
   },
