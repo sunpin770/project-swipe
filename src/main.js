5ae93eb001ea2ad3f47e2e973b11a07c25df1f03
sunpin770 <246813579rpc@gmail.com>
2025-05-16 10:46:20 -0400

    beh..!

--- src/main.js
+++ src/main.js
@@ -23,8 +23,8 @@ const game = new Engine({
 
 
 const calculateExPixelConversion = (screen) => {
-  const origin = screen.worldToScreenCoordinates(new Vector.Zero);
-  const singlePixel = screen.worldToScreenCoordinates(Vector(1, 0)).sub(origin);
+  const origin = screen.worldToScreenCoordinates(Vector.Zero);
+  const singlePixel = screen.worldToScreenCoordinates(new Vector(1, 0)).sub(origin);
   const pixelConversion = singlePixel.x;
   document.documentElement.style.setProperty('--pixel-conversion', pixelConversion.toString());
 }
